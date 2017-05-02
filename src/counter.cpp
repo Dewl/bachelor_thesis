55c753a6614766d6588d7f8cb7ec928145495d92
Khoi Hoang <hkhoi@outlook.com>
2017-05-03 00:47:10 +0700

    add documentation for counter module

--- src/counter.cpp
+++ src/counter.cpp
@@ -1,3 +1,29 @@
+/* Copyright (C) 
+ * 2017 - Khoi Hoang
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * 
+ */
+
+/**
+ * @file counter.cpp
+ * @brief The implementation for counter.h
+ * @author Khoi Hoang
+ * @version 1.0
+ * @date 2017-05-03
+ */
+
 #include "counter.h"
 
 Counter::Counter(bool _hor, int _bound1, int _bound2)
